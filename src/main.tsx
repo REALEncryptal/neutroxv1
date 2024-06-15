96f0cb3d9b81a415c9ecba3b3e82b4231b8e8203
realencryptal <realencryptal@gmail.com>
2024-06-14 20:13:13 -0700

    a

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
     element: <><Home/></>,
   },
 ]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
